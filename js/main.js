af26773313f28af64374afbc1cf6b1a398479b68
Ouss Bahri <bahrioussama84@gmail.com>
2020-11-05 19:45:33 +0200

    Updates

---
+++ js/main.js
@@ -0,0 +1,31 @@
+//selection of The DOM
+const menuBtn = document.querySelector('.menu-button');
+const menu = document.querySelector('.menu');
+const menuNav = document.querySelector('.menu-nav');
+const navItem = document.querySelectorAll('.nav-item');
+const menuBranding = document.querySelector('.menu-branding');
+
+//the initial state of the menu
+let showMenu = false;
+
+menuBtn.addEventListener('click', toggleMenu);
+
+function toggleMenu (){
+    if (!showMenu){
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItem.forEach(item => item.classList.add('show'))
+        //set menu state
+        showMenu=true;
+    }else{
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItem.forEach(item => item.classList.remove('show'));
+        //set menu state
+        showMenu=false;
+    }
+}
